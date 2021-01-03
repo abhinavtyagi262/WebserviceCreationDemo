89e0ce8ecd61f337ed086e2ec1bc5a96cc028aa6
Abhinav Tyagi <61839001+abhinavtyagi262@users.noreply.github.com>
2021-01-03 20:48:21 +0530

    Webservice Url: http://localhost:8080/WebserviceCreationDemo/ApplicationPath/WebservicePath/
    Content-Type: application/json
    Request Json: {"name":"name"}
    Resonse Json: {"name":"name"}

---
+++ WebserviceCreationDemo/src/main/java/com/abhinav/ApplicationPathClass.java
@@ -0,0 +1,9 @@
+package com.abhinav;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/ApplicationPath")
+public class ApplicationPathClass extends Application {
+
+}
---
+++ WebserviceCreationDemo/src/main/java/com/abhinav/WebserviceCreationClass.java
@@ -0,0 +1,51 @@
+package com.abhinav;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+@Path("/")
+public class WebserviceCreationClass {
+	
+	private static Logger log = LoggerFactory.getLogger(WebserviceCreationClass.class.getName());
+	
+	@Path("WebservicePath")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String WebserviceCreationMethod(InputStream incomingData) {
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+			String line = null;
+			while((line = in.readLine())!=null) {
+				stringBuilder.append(line);
+			}
+		}	
+		catch(Exception e) {
+			log.error("Error occurred while reading incoming data: " + e );
+		}
+		
+		String requestString = new String(stringBuilder.toString());
+		log.info("Request is: " + requestString);
+		System.out.println("Request is: " + requestString);
+		
+		String responseString = requestString;
+		log.info("Response is: " + requestString);
+		System.out.println("Response is: " + requestString);
+		
+		return responseString;
+	}
+}
